4e4bbcb79429dfb6bb543e8c0b3cf132e278ce8d
matheuscidral <matheus.costa.cidral@gmail.com>
2023-08-17 19:12:41 -0300

    tirando ponto e vírgula

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import express from "express";
-const app = express();
+import express from "express"
+const app = express()
 
 app.get("/api/:userId/unknow/:personId", (req, res) => {
-    const nome = req.query.personId;
-    const id = req.query.userId;
+    const nome = req.query.personId
+    const id = req.query.userId
 
     res.json({
         message: `queryParam ${nome}` + ` id ${id}`
-    });
-});
+    })
+})
 
 app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
-});
+    console.log("Example app listening on port 3000!")
+})
